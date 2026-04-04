73f0afefdbdd3fd2e3a1b0720646dd29c23d90b1
dennischee512 <dennischee512@gmail.com>
2026-04-04 07:27:18 +0000

    test(controller): dashboards JSON endpoint spec (failing)

---
+++ spec/requests/dashboards_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Dashboards', type: :request do
+  describe 'GET /dashboards' do
+    it 'returns usage_rate and bookings_data JSON' do
+      get dashboards_path, as: :json
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body)).to include(
+        'usage_rate' => 75.0,
+        'bookings_data' => hash_including('2026-04-01' => 2)
+      )
+    end
+  end
+end
