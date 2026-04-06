cfdc1cc01e4bcf838cd1fe5a9d651694fa7ffb71
TyPurp <ltypurple0099@gmail.com>
2026-04-06 07:00:03 +0000

    Update search controller and index to read quipment from database

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,16 +2,24 @@ class SearchController < ApplicationController
   def index
     @query = params[:q]
     
-    # Mock search results
-    case @query&.downcase
-    when 'projector'
-      @results = ['Projector A (Available)', 'Projector B (Unavailable)']
-    when 'room', 'venue'
-      @results = ['Room A (12 slots)', 'Room B (8 slots)']
-    when 'speaker'
-      @results = ['Wireless Speaker', 'Conference Speaker']
+    # Mock search results --> (p) change to read from database
+    # case @query&.downcase
+    # when 'projector'
+    #   @results = ['Projector A (Available)', 'Projector B (Unavailable)']
+    # when 'room', 'venue'
+    #   @results = ['Room A (12 slots)', 'Room B (8 slots)']
+    # when 'speaker'
+    #   @results = ['Wireless Speaker', 'Conference Speaker']
+    # else
+    #   @results = []
+    # end
+
+    
+    if @query.present?   #if user enter search
+      @results =Equipment.where(name: @query.downcase)
     else
-      @results = []
+      @results =[]
     end
+
   end
 end
