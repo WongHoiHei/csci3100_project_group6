c5b34c9d7be7474bf136ebc48406af029ae7a2df
chloe050627 <hochitung0627@gmail.com>
2026-04-10 19:30:14 +0800

    search bar problem of history page solved!

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -56,16 +56,36 @@ class BookingsController < ApplicationController
   end
 
   def index
-    @bookings = current_user.bookings
+    @bookings = current_user.bookings.order(created_at: :desc)
+    @search_query = params[:q]
+    
+    if @search_query.present?
+      query = "%#{@search_query.downcase}%"
+      equipment_ids = Equipment.where("LOWER(name) LIKE ?", query).pluck(:id)
+      venue_ids = Venue.where("LOWER(name) LIKE ?", query).pluck(:id)
+      
+      if equipment_ids.any? || venue_ids.any?
+        @bookings = @bookings.where(
+          "bookable_type = 'Equipment' AND bookable_id IN (:equipment_ids) OR bookable_type = 'Venue' AND bookable_id IN (:venue_ids)",
+          equipment_ids: equipment_ids,
+          venue_ids: venue_ids
+        )
+      else
+        @bookings = @bookings.where(id: nil)
+      end
+    end
   end
 
   def destroy
     @booking = Booking.find(params[:id])
     if @booking.user == current_user
-      BookingMailer.deletion(@booking).deliver_later
-      @booking.destroy
-      sender_link = view_context.mail_to("venueandequipmentbooking@gmail.com", "venueandequipmentbooking@gmail.com")
-      redirect_to bookings_path, notice: "Booking deleted. Email sent from #{sender_link}".html_safe
+      if @booking.destroy
+        BookingMailer.deletion(@booking).deliver_now
+        sender_link = view_context.mail_to("venueandequipmentbooking@gmail.com", "venueandequipmentbooking@gmail.com")
+        redirect_to bookings_path, notice: "Booking deleted. Email sent from #{sender_link}".html_safe
+      else
+        redirect_to bookings_path, alert: "Unable to cancel booking."
+      end
     else
       redirect_to bookings_path, alert: "Unauthorized"
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :bookings, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
